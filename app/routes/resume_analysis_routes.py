006eccf183c352afe52db7e97f4a8a59906ff450
Davi de Oliveira <olidavi031130@gmail.com>
2025-10-23 13:14:17 -0300

    feat: include authentication to resume_analysis endpoint

--- app/routes/resume_analysis_routes.py
+++ app/routes/resume_analysis_routes.py
@@ -1,13 +1,17 @@
 # app\routes\analyze_resume_routes.py
-from fastapi import APIRouter, HTTPException, UploadFile, File
+from fastapi import APIRouter, HTTPException, UploadFile, File, Depends
 from fastapi.responses import JSONResponse
+from app.models.user import User
 from app.services.resume_analysis_services import analyze_resume_service
+from app.dependencies.security import verify_token
 
 analyze_resume_router = APIRouter(prefix="/analyze-resume", tags=["resume-analysis"])
 
 
 @analyze_resume_router.post("/")
-async def analyze_resume(file: UploadFile = File(...)):
+async def analyze_resume(
+    file: UploadFile = File(...), current_user: User = Depends(verify_token)
+):
     """
     Faz análise do resumo enviado em .pdf e retorna para o usuário.
     """
@@ -17,10 +21,10 @@ async def analyze_resume(file: UploadFile = File(...)):
     try:
         # Lê o conteúdo do arquivo
         contents = await file.read()
-        
+
         # Chama o service para processar a análise
         result = await analyze_resume_service(contents, file.filename)
-        
+
         return JSONResponse(result)
 
     except ValueError as e:
