7dc64606bf7ae8bf3cdd1749cd50f7c01d27b8ff
Davi de Oliveira <olidavi031130@gmail.com>
2025-10-24 19:05:00 -0300

    feat: update verify_token dependency to check if a token is blacklisted

--- app/dependencies/security.py
+++ app/dependencies/security.py
@@ -2,6 +2,7 @@ from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from app.models.user import User
+from app.core.security import is_token_blacklisted
 from app.dependencies.database import get_db
 from jose import jwt, JWTError
 from app.core.config import settings
@@ -10,21 +11,35 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login-form")
 
 
 def verify_token(
-    token: str = Depends(oauth2_scheme), session: Session = Depends(get_db)
+    token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
 ):
     try:
+        # Verifica se o token ta na blacklist
+        if is_token_blacklisted(token, db):
+            raise HTTPException(
+                status_code=401,
+                detail="Token revoked"
+            )
+        
         payload = jwt.decode(
             token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
         )
-        user_id = int(payload.get("sub"))
+        user_id: str = payload.get("sub")
         if user_id is None:
-            raise HTTPException(status_code=401, detail="Invalid token")
-
-        user = session.query(User).filter(User.id == user_id).first()
-        if not user:
-            raise HTTPException(status_code=404, detail="User not found")
-
-        return user
-
+            raise HTTPException(
+                status_code=401,
+                detail="Não foi possível validar as credenciais",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
     except JWTError:
-        raise HTTPException(status_code=401, detail="Token is invalid or expired")
+        raise HTTPException(status_code=401, detail="Token é inválido ou está expirado")
+    
+    user = db.query(User).filter(User.id == user_id).first()
+    if user is None:
+        raise HTTPException(
+            status_code=401,
+            detail="Não foi possível validar as credenciais",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+    return user
+    
